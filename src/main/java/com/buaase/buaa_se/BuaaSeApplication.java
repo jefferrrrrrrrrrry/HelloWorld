086b8bb5d9f859c9e7efaad21ba7ba374f7b998d
1 <21371202@buaa.edu.cn>
2024-03-19 08:44:56 +0800

    init

---
+++ src/main/java/com/buaase/buaa_se/BuaaSeApplication.java
@@ -0,0 +1,13 @@
+package com.buaase.buaa_se;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuaaSeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuaaSeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/buaase/buaa_se/BuaaSeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buaase.buaa_se;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuaaSeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
